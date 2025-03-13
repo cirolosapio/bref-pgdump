573e3cab051ae3fc77b38d3e81c25b7b8e0a364f
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-03-13 10:32:56 +0000

    add backup command to console for database version retrieval

---
+++ app/Console/Commands/backup.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class backup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info(shell_exec('pg_dump --version'));
+    }
+}
