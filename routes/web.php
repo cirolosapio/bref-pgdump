4b3311d52609d4e99f58ef3807fdc226d8113fdf
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-03-14 11:01:47 +0000

    update cache configuration to use array store and add download route for streaming log files

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', fn() => shell_exec('pg_dump --version'));
+Route::get('test', fn () => shell_exec('pg_dump --version'));
+
+Route::get('download', function () {
+    $size = request()->input('size', 1000);
+    $content = str_repeat('A', $size);
+
+    return response()->streamDownload(static function () use ($content): void { echo $content; }, 'test.log');
+});
