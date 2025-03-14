5858a238fe9f95f26e01fd60bdea08f6ce25f767
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-03-14 11:16:17 +0000

    add validation for download size in web route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('/', function () {
 Route::get('test', fn () => shell_exec('pg_dump --version'));
 
 Route::get('download', function () {
+    request()->validate(['size' => 'int|max:6289443']);
+
     $size = request()->input('size', 1000);
     $content = str_repeat('A', $size);
 
