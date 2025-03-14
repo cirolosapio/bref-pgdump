2d632cb1adcda97057d82a5e15200bbd01e685a6
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-03-14 11:27:42 +0000

    refactor download route to validate size input and set default value

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,8 @@ Route::get('/', function () {
 Route::get('test', fn () => shell_exec('pg_dump --version'));
 
 Route::get('download', function () {
-    request()->validate(['size' => 'int|max:6289443']);
+    $size = (int) request()->validate(['size' => 'integer|max:6289443'])['size'] ?? 1000;
 
-    $size = request()->input('size', 1000);
     $content = str_repeat('A', $size);
 
     return response()->streamDownload(static function () use ($content): void { echo $content; }, 'test.log');
